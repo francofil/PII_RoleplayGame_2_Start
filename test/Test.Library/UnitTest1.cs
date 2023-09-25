b6edb9a61d07486f5f6a7b5d74411ae95870c080
maruuroman <maria.roman@correo.ucu.edu.uy>
2023-09-25 10:21:29 -0300

    test

--- test/Test.Library/UnitTest1.cs
+++ test/Test.Library/UnitTest1.cs
@@ -2,6 +2,7 @@ using System.Reflection.PortableExecutable;
 using NUnit.Framework;
 using RoleplayGame;
 
+
 namespace Test.Library
 {
     public class Tests
@@ -26,6 +27,18 @@ namespace Test.Library
             bool output = Health==100;
             Assert.AreEqual(output, expected);
             
+        }
+        [Test]
+        public void DefenseValue_ReturnsNonNegativeValue()
+        {
+            // Arrange
+            Item item = new Item(); // Reemplaza 'Item' con tu implementación real si es necesario.
+
+            // Act
+            int defenseValue = item.DefenseValue();
+
+            // Assert
+            Assert.IsTrue(defenseValue >= 0, "DefenseValue should return a non-negative value.");
         }
     }
 }
