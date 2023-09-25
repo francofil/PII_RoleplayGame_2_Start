914dc122d0cdf36f3477afe36be729cf3e9debe0
maruuroman <maria.roman@correo.ucu.edu.uy>
2023-09-25 12:04:15 -0300

    agrego testeos

--- test/Test.Library/UnitTest1.cs
+++ test/Test.Library/UnitTest1.cs
@@ -25,13 +25,12 @@ namespace Test.Library
             bool input = gimli.Health == 100;
             bool expected = gimli.Health == 100;
             bool output = gimli.Health == 100;
-            Assert.That(output, Is.EqualTo(expected));
-            
+            Assert.That(output, Is.EqualTo(expected)); 
         }
 
 
         [Test]
-        public void DefenseValue_ReturnsNonNegativeValue()
+        public void DefenseValue_ReturnsNonNegativeValue1()
         {
             
             Armor item = new Armor(); 
@@ -39,6 +38,69 @@ namespace Test.Library
             int defenseValue = item.DefenseValue();
 
             Assert.IsTrue(defenseValue >= 0, "DefenseValue should return a non-negative value.");
+
         }
+
+
+        [Test]
+        public void DefenseValue_ReturnsNonNegativeValue2()
+        {
+            
+            Helmet item = new Helmet(); 
+
+            int defenseValue = item.DefenseValue();
+
+            Assert.IsTrue(defenseValue >= 0, "DefenseValue should return a non-negative value.");
+
+        }
+
+        [Test]
+        public void DefenseValue_ReturnsNonNegativeValue3()
+        {
+            
+            Shield item = new Shield(); 
+
+            int defenseValue = item.DefenseValue();
+
+            Assert.IsTrue(defenseValue >= 0, "DefenseValue should return a non-negative value.");
+
+        }
+
+        [Test]
+        public void DefenseValue_ReturnsNonNegativeValue4()
+        {
+            
+            Spell item = new Spell(); 
+
+            int defenseValue = item.DefenseValue();
+
+            Assert.IsTrue(defenseValue >= 0, "DefenseValue should return a non-negative value.");
+
+        }
+
+        [Test]
+        public void DefenseValue_ReturnsNonNegativeValue5()
+        {
+            
+            SpellsBook item = new SpellsBook(); 
+
+            int defenseValue = item.DefenseValue();
+
+            Assert.IsTrue(defenseValue >= 0, "DefenseValue should return a non-negative value.");
+
+        }
+
+         [Test]
+        public void DefenseValue_ReturnsNonNegativeValue6()
+        {
+            
+            Staff item = new Staff(); 
+
+            int defenseValue = item.DefenseValue();
+
+            Assert.IsTrue(defenseValue >= 0, "DefenseValue should return a non-negative value.");
+
+        }
+        
     }
 }
